70ff2a0c83059138a0ed67509914cebbba96a530
JulienPeloton <peloton@lal.in2p3.fr>
2026-06-03 08:35:13 +0200

    PEP8

--- apps/summary.py
+++ apps/summary.py
@@ -818,7 +818,11 @@ def is_blazar(pdfs):
     Works for old schema (_m0), and low/high states with new schema
     """
     # Handle schema migration, and low/high states
-    cols = ["d:blazar_stats_m0", "d:blazar_stats_instantness_low", "d:blazar_stats_instantness_high"]
+    cols = [
+        "d:blazar_stats_m0",
+        "d:blazar_stats_instantness_low",
+        "d:blazar_stats_instantness_high",
+    ]
     for col in cols:
         payload = pdfs[col].to_numpy()
         if np.any(payload != -1):
